473a2496f48b3a6b139998f6f0f38c6c11513461
DJ Shin <djatlienriseup@gmail.com>
2019-05-30 12:51:54 -0400

    setting state for album list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import axios from 'axios'
 
 export default class AlbumList extends Component {
+
+componentWillMount(){
+  axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({ albums: response.data}));
+}
+
   render() {
     return (
       <View>
