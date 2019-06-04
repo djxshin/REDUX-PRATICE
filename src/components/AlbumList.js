e201bf9eb907a80e414581abd0d34c64c44b5cda
DJ Shin <djatlienriseup@gmail.com>
2019-06-04 01:45:14 -0400

    image added + thumbnail added styles added + button added

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {View } from 'react-native'
+import {View, ScrollView } from 'react-native'
 import axios from 'axios'
 
 import AlbumDetail from "./AlbumDetail";
@@ -25,9 +25,9 @@ renderAlbums(){
     console.log(this.state);
 
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     )
   }
 }
